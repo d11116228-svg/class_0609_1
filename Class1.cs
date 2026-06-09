026fc9fceb677086fa2f37458cb7cc171aaa6739
d11116228-svg <d11116228@gs.takming.edu.tw>
2026-06-09 16:13:23 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Student
+{
+    public int Mid;
+    public int Final;
+
+    public Student(int m, int f)
+    {
+        Mid = m;
+        Final = f;
+    }
+
+    public int Plus(int a, int b)
+    {
+        int temp;
+        temp = (Mid + Final) / 2;
+        return temp;
+    }
+
+    public static Student operator +(Student a, Student b)
+    {
+        Student temp = new Student(0, 0);
+        temp.Mid = (a.Mid + b.Mid) / 2;
+        temp.Final = (a.Final + b.Final) / 2;
+        return temp;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Class_0609_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(12, 27);
+            Student s2 = new Student(60, 80);
+            Student savg = s1 + s2;
+            Console.WriteLine(savg.Mid);
+            Console.WriteLine(savg.Final);
+        }
+    }
+}
